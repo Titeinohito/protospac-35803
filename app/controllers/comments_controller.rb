58e90cd11b991f69daf3d7404a1e2e3c5b48d2d5
Titeinohito <84887486+Titeinohito@users.noreply.github.com>
2021-06-25 11:12:46 +0900

    不要箇所削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,14 +10,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    #@prototype = Prototype.new
-    #binding.pry
-    #@prototype = Prototype.find(params[:prototype_id])
-    #@comment = @prototype.comments.new(comment_params)
-    #binding.pry
-    #@comment.save
     Comment.create(comment_params)
-    #binding.pry
     if Comment.create
       redirect_to controller: :prototypes, action: :show, id: params[:prototype_id]
     else
@@ -28,7 +21,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    #binding.pry
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 end
